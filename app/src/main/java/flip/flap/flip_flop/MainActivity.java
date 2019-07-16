4250e59fe7bc9f884f0167c604683ec3e973baf5
Zml99 <malinares99@gmail.com>
2019-07-16 19:24:05 -0400

    Fotos de naipes

--- app/src/main/java/flip/flap/flip_flop/MainActivity.java
+++ app/src/main/java/flip/flap/flip_flop/MainActivity.java
@@ -1,13 +1,27 @@
 package flip.flap.flip_flop;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //To hide navigation bar and notifications bar.
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+
+
     }
 }
